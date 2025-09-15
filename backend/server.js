f5485e222999794720e3bb8b70492978046c8273
G2Me-B <123091013+G2Me-B@users.noreply.github.com>
2025-09-15 09:28:56 +0000

    critical changes

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import cors from 'cors';
 import "dotenv/config";
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { sql } from './config/db.js';
-import { aj } from './lib/arcjet.js';
+import {
+    sql
+} from './config/db.js';
+import {
+    aj
+} from './lib/arcjet.js';
 import path from "path"
 import productRoutes from './routes/productRoutes.js';
 
@@ -14,45 +18,61 @@ const __dirname = path.resolve();
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); // For security headers
+app.use(helmet({
+    contentSecurityPolicy: false,
+})); // For security headers
 app.use(morgan('dev')); // For logging requests
 
 // Apply arcjet limits to routes
-app.use(async(req, res, next) => {
-try {
-    const decision =await aj.protect(req,{
-        requested:1 // number of requests
-    })
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, {
+            requested: 1 // number of requests
+        })
 
-    if(decision.isDenied()){
-        if(decision.isRateLimited()){ 
-            return res.status(429).json({success: false, message: "Too many requests - Rate limit exceeded"});
-        } else if(decision.reason.isBot()){
-            res.status(403).json({success: false, message: "Access denied - Bot detected"});
-        } else{
-            res.status(403).json({success: false, message: "Access denied"});
+        if (decision.isDenied()) {
+            if (decision.isRateLimited()) {
+                return res.status(429).json({
+                    success: false,
+                    message: "Too many requests - Rate limit exceeded"
+                });
+            } else if (decision.reason.isBot()) {
+                res.status(403).json({
+                    success: false,
+                    message: "Access denied - Bot detected"
+                });
+            } else {
+                res.status(403).json({
+                    success: false,
+                    message: "Access denied"
+                });
+            }
+            return
         }
-        return
-    }
 
-    //  check for spoof bot
-    if(decision.results.some((result)=>result.reason.isBot()&& result.reason.isSpoofed())){
-        res.status(403).json({error: "Access denied - Spoofed bot detected" });
-        return; 
+        //  check for spoof bot
+        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+            res.status(403).json({
+                error: "Access denied - Spoofed bot detected"
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        console.log("Arcjet error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+        next(error)
     }
-    next();
-} catch (error) {
-    console.log("Arcjet error:", error);
-    res.status(500).json({success: false, message: "Internal server error" });
-    next(error)  
-}
 })
 
 app.use('/api/products', productRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")))
-    app.get("*", (req, res) => {
+    app.get("", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 }
